58a7cb322fc3474798a3051cfe0ecf340c4c2589
Metea <metea@example.com>
2026-03-05 19:50:07 +0900

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ← ルート公開用
+  base: '/conan-deck/', // GitHub Pages用
 })
